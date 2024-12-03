1dd1d60dcd3c6de1989f23184d89a2167a7e3e67
Troy Fullwood <everyfan3417@gmail.com>
2024-12-03 13:44:07 -0500

    day 3 won

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! Advent of Code 2024 by Troy F.
 
-use aoc2024::{day01::day01, day02::day02};
+use aoc2024::{day01::day01, day02::day02, day03::day03};
 use clap::Parser;
 
 fn main() {
@@ -8,6 +8,7 @@ fn main() {
     match args.day {
         1 => day01(),
         2 => day02(),
+        3 => day03(),
         _ => panic!("bad day"),
     }
 }
